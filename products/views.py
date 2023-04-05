20a174320c3072a2113e80a871aa650e3cdf3b6b
Aravind L S <aravind.ls@accubits.com>
2023-04-05 16:39:49 +0530

    views updated

--- products/views.py
+++ products/views.py
@@ -23,10 +23,15 @@ class FilterProductVIew(ListAPIView):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
-        category_ids = self.request.query_params.getlist('category')
         queryset = Product.objects.all()
+        try:
+            category_ids = self.request.GET.get('category')
+            category_ids = list(map(int, category_ids.split(',')))
+            
 
-        if category_ids:
-            queryset = queryset.filter(category__id__in=category_ids)
+            if category_ids:
+                queryset = queryset.filter(category__id__in=category_ids)
+        except:
+            pass
 
         return queryset
