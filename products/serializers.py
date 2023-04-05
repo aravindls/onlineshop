46e45d4ddf4da4075705d1d246760078ad439335
Aravind L S <aravind.ls@accubits.com>
2023-04-05 16:52:46 +0530

    serializer updated

--- products/serializers.py
+++ products/serializers.py
@@ -9,7 +9,17 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    
+    category = serializers.SerializerMethodField()
+    sub_category = serializers.SerializerMethodField()
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = ('name', 'description','price','category','sub_category')
+        
+    def get_category(self, obj):
+        print(obj.category.values_list())
+        category =dict(obj.category.values_list())
+        return category
+    
+    def get_sub_category(self, obj):
+        sub_category =dict(obj.subcategory.values_list('id','name'))
+        return sub_category
